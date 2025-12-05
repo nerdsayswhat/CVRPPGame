277b28cc079b627c90c7ceebf4966645d663c474
Isaro1971 <17378397031@163.com>
2025-12-05 00:05:37 +0000

    Hands model&animation(replaced controllers)
    were used to grab more naturally

---
+++ Assets/Hands/Girl's Hands/Scripts/AnimateHandOnInput.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class AnimateHandOnInput : MonoBehaviour
+{
+    public InputActionProperty triggerAnimationAction;
+    public InputActionProperty gripAnimationAction;
+    public Animator handAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float triggerValue = triggerAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+
+        float gripValue = gripAnimationAction.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+    }
+}
